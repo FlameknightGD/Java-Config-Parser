fbf3221b6f28a28a0966612a208ef93ff7c3dc46
Flame <61699155+FlameknightGD@users.noreply.github.com>
2021-06-24 18:07:07 +0200

    Update ConfigParser.java
    Added documentation and setFilePath method

--- ConfigParser.java
+++ ConfigParser.java
@@ -6,36 +6,53 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.HashMap;
 
+/**
+ * A simple tool based off the HashMap class that can be used to easily write
+ * and read configuration files of your Java project. You can find my other
+ * projects at github.com/FlameknightGD.
+ * 
+ * @version v1.0.1
+ * @author FlameknightGD
+ * @see java.util.HashMap
+ */
+
 public class ConfigParser extends HashMap<String, String> {
 	// Global Variables
 
 	/**
-	 * Serial Version UID that is used as an identifier for the ConfigParser class
+	 * Serial Version UID that is used as an identifier for the ConfigParser class.
+	 * 
+	 * @since v1.0.0
 	 */
 	private static final long serialVersionUID = 5979389544685269824L;
 
 	/**
-	 * The file that is going to be used by the ConfigParser to save and read information
+	 * The file that is going to be used by the ConfigParser to save and read
+	 * information.
+	 *
+	 * @since v1.0.0
 	 */
 	File configFile;
 
 	// Constructors
 
 	/**
-	 * Constructer for the ConfigParser class using java.io.File
+	 * Constructer for the ConfigParser class using the actual file.
 	 * 
-	 * @param File 'configFile' - The file that is going to be used by the
-	 *             ConfigParser to save and read information
+	 * @param configFile The file that is going to be used by the ConfigParser to
+	 *                   save and read information
+	 * @since v1.0.0
 	 */
 	public ConfigParser(File configFile) {
 		setConfigFile(configFile);
 	}
 
 	/**
-	 * Constructer for the ConfigParser class using a String as the file path
+	 * Constructer for the ConfigParser class using the file path.
 	 * 
-	 * @param String 'filePath' - The path of the file that is going to be used by
-	 *               the ConfigParser to save and read information
+	 * @param filePath The path of the file that is going to be used by the
+	 *                 ConfigParser to save and read information.
+	 * @since v1.0.0
 	 */
 	public ConfigParser(String filePath) {
 		setConfigFile(new File(filePath));
@@ -44,10 +61,17 @@ public class ConfigParser extends HashMap<String, String> {
 	// Main Methods
 
 	/**
-	 * Method used to write the config file with the current keys and values of the
-	 * ConfigParser object
+	 * Writes the content of the ConfigParser object into an external file.
 	 * 
-	 * @throws IOException if there any are issues with the file
+	 * <p>
+	 * When called, the method {@link #write()} will start a for each loop to write
+	 * each key and its corresponding value into an external file, using a
+	 * StringBuffer and the global variable '{@link #configFile}'.
+	 * 
+	 * @throws IOException if the file can't be found
+	 * @since v1.0.0
+	 * @see java.lang.StringBuffer
+	 * @see java.nio.file.Files
 	 */
 	public void write() throws IOException {
 		StringBuffer fileContent = new StringBuffer();
@@ -58,38 +82,47 @@ public class ConfigParser extends HashMap<String, String> {
 	}
 
 	/**
-	 * Method used to read a specific value of the config file with the current keys
-	 * and values of the ConfigParser object
+	 * Reads the content of the configuration file into the ConfigParser object
 	 * 
-	 * @throws IOException if there are any issues with the file
+	 * <p>
+	 * When called, the method {@link #read()} will clear the content of the
+	 * ConfigParser object and use the Files.readAllBytes method to get the Bytes of
+	 * the configuration file and convert said Bytes to a string. This string will
+	 * be split with the String.split() method and all keys and their corresponding
+	 * values will be added to the ConfigParser object via the put method of the
+	 * HashMap class.
+	 * 
+	 * @throws IOException if the file can't be found
+	 * @since v1.0.0
+	 * @see java.nio.charset.StandardCharsets
+	 * @see java.nio.file.Files
 	 */
 	public void read() throws IOException {
 		this.clear();
 		String fileContent = new String(Files.readAllBytes(configFile.toPath()), StandardCharsets.UTF_8);
-		for (String line : fileContent.split("\n"))
-        {
+		for (String line : fileContent.split("\n")) {
 			String[] splitLine = line.split("=");
 			this.put(splitLine[0], splitLine[1]);
-        }
+		}
 	}
 
 	// Getters
 
 	/**
-	 * Method to get the file that is being used by the ConfigParser to save and
-	 * read information
+	 * Gets the currently selected configuration file.
 	 * 
 	 * @return File that is currently selected by the ConfigParser
+	 * @since v1.0.0
 	 */
 	public File getConfigFile() {
 		return this.configFile;
 	}
 
 	/**
-	 * Method to get the file that is being used by the ConfigParser to save and
-	 * read information
+	 * Gets the path of the currently selected configuration file.
 	 * 
 	 * @return File that is currently selected by the ConfigParser
+	 * @since v1.0.0
 	 */
 	public String getFilePath() {
 		return this.configFile.getPath();
@@ -98,13 +131,24 @@ public class ConfigParser extends HashMap<String, String> {
 	// Setters
 
 	/**
-	 * Method to change the file that is being used by the ConfigParser to save and
-	 * read information
+	 * Sets the file that is being used by the ConfigParser object.
 	 * 
-	 * @param File 'configFile' - The file that is going to be used by the
-	 *             ConfigParser to save and read information
+	 * @param configFile The file that is going to be used by the ConfigParser to
+	 *                   save and read information
+	 * @since v1.0.0
 	 */
 	public void setConfigFile(File configFile) {
 		this.configFile = configFile;
+	}
+
+	/**
+	 * Sets the path of the file that is being used by the ConfigParser object.
+	 * 
+	 * @param filePath The path of the file that is going to be used by the
+	 *                 ConfigParser to save and read information
+	 * @since v1.0.1
+	 */
+	public void setFilePath(String filePath) {
+		this.configFile = new File(filePath);
 	}
 }
